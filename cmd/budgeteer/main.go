ef69dcc7dd9cfe1b652e6e57eafc53a6a5c21ef1
Christo Swanepoel <24827177@sun.ac.za>
2024-12-11 13:58:09 +0200

    feat: migrated to gorm

--- cmd/budgeteer/main.go
+++ cmd/budgeteer/main.go
@@ -2,77 +2,54 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"log"
 
 	"github.com/christo-sw/budgeteer/internal/database"
-	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 var (
-	categories = []database.Category{
-		{Category: "Tithe"},
-		{Category: "Tax"},
-		{Category: "Medical"},
-		{Category: "Pension"},
-		{Category: "Internet"},
-		{Category: "Clothing"},
-		{Category: "Cellphone"},
-		{Category: "Car Insurance"},
-		{Category: "Car Licence"},
-		{Category: "Rent"},
-		{Category: "Obsidian"},
-		{Category: "1Password"},
-		{Category: "uSchool"},
-		{Category: "Petrol/Parking"},
-		{Category: "Eating Out"},
-		{Category: "Offering"},
-		{Category: "Gym"},
-		{Category: "Banking Fees"},
-		{Category: "Travel"},
-		{Category: "Christmas Gifts"},
-		{Category: "Birthday Gifts"},
-		{Category: "Vacation"},
-		{Category: "Office Equipment"},
-		{Category: "XTO Bespoke"},
-		{Category: "Sundry"},
+	categories = []string{
+		"Tithe",
+		"Tax",
+		"Medical",
+		"Pension",
+		"Internet",
+		"Clothing",
+		"Cellphone",
+		"Car Insurance",
+		"Car Licence",
+		"Rent",
+		"Obsidian",
+		"1Password",
+		"uSchool",
+		"Petrol/Parking",
+		"Eating Out",
+		"Offering",
+		"Gym",
+		"Banking Fees",
+		"Travel",
+		"Christmas Gifts",
+		"Birthday Gifts",
+		"Vacation",
+		"Office Equipment",
+		"XTO Bespoke",
+		"Sundry",
 	}
 )
 
 func main() {
-	// Open connection to SQLite database
-	db, err := sqlx.Open("sqlite3", "./budget.db")
+	db, err := gorm.Open(sqlite.Open("budget.db"), &gorm.Config{})
 	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	// Test the connection
-	if err := db.Ping(); err != nil {
-		panic(fmt.Sprintf("Cannot connect to database: %v\n", err))
+		log.Fatalf("Failed to connect to database: %v", err)
 	}
 
-	schema, err := os.ReadFile("internal/database/schema.sql")
-	if err != nil {
-		panic(err)
-	}
+	db.AutoMigrate(&database.Transaction{})
+	db.AutoMigrate(&database.Balance{})
 
-	db.MustExec(string(schema))
-
-	fmt.Println("Successfully connected to DB")
-
-	store := database.Store{DB: db}
-
-	balance, err := store.CreateBalance(database.Category{Category: "Tithe"}, 300000)
-	if err != nil {
-		panic(err)
-	}
-
-	balance, err = store.GetLatestBalance(database.Category{Category: "Tithe"})
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Latest balances:")
-	fmt.Printf("%+v\n", balance)
+	var balances []database.Balance
+	db.Find(&balances)
+	fmt.Printf("Balance: %+v\n", balances)
 }
--- internal/database/database.go
+++ internal/database/database.go
@@ -3,65 +3,25 @@ package database
 import (
 	"time"
 
-	"github.com/jmoiron/sqlx"
+	"gorm.io/gorm"
 )
 
-type Store struct {
-	DB *sqlx.DB
-}
-
 type Category struct {
-	Category string `db:"category"`
+	gorm.Model
+	Category string
 }
 
 type Balance struct {
-	Date        time.Time `db:"date"`
-	Category    Category  `db:"category"`
-	AmountCents int64     `db:"amount_cents"`
+	gorm.Model
+	Date        time.Time
+	Category    string
+	AmountCents int64
 }
 
 type Transaction struct {
-	ID          int64     `db:"id"`
-	Date        time.Time `db:"date"`
-	AmountCents int64     `db:"amount_cents"`
-	Description string    `db:"description"`
+	gorm.Model
+	ID          int64
+	Date        time.Time
+	AmountCents int64
+	Description string
 }
-
-func (s Store) GetLatestBalance(category Category) (*Balance, error) {
-	balance := Balance{}
-
-	err := s.DB.Get(&balance, "SELECT * FROM balances WHERE category=$1 ORDER BY date DESC LIMIT 1",
-		category.Category)
-	if err != nil {
-		return nil, err
-	}
-
-	return &balance, nil
-}
-
-func (s Store) CreateBalance(category Category, amount int64) (*Balance, error) {
-	tx, err := s.DB.Begin()
-	if err != nil {
-		return nil, err
-	}
-
-	_, err = tx.Exec("INSERT INTO balances (date, category, amount_cents) VALUES ($1, $2, $3)",
-		time.Now(), category.Category, amount)
-	if err != nil {
-		return nil, err
-	}
-
-	err = tx.Commit()
-	if err != nil {
-		return nil, err
-	}
-
-	balance, err := s.GetLatestBalance(category)
-	if err != nil {
-		return nil, err
-	}
-
-	return balance, nil
-}
-
-func (s Store) CreateCategory() {}
